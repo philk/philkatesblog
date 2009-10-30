166e2d300fe22234c6d96903b9c40a16cec332e2
Phil Kates <me@philkates.com>
2009-10-30 15:23:18 -0400

    Changed navigation Completely altered post and default layouts so that the sidebar only appears on the main page.

--- _helpers.rb
+++ _helpers.rb
@@ -6,3 +6,9 @@ FEED_URL = "http://philkates.com/atom.xml"
 # def rssfeed(feed_url)
 #   feed = Feedzirra::Feed.fetch_and_parse(feed_url).entries
 # end
+
+module Helpers
+  def post_arrow(post, direction)
+    "<a href='#{h(post.url)}'>#{direction}</a>"
+  end
+end
