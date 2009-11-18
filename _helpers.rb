4759cc306e0620798f5f3419a2572a747a23a7f1
Phil Kates <me@philkates.com>
2009-11-18 16:24:35 -0500

    Added page_link helper for simpler prev/next urls

--- _helpers.rb
+++ _helpers.rb
@@ -1,15 +1,11 @@
-# require "feedzirra"
-
-# TODO: Change to feedburner
 # FEED_URL = "http://philkates.com/atom.xml"
 FEED_URL = "http://feeds.feedburner.com/philkates"
-
-# def rssfeed(feed_url)
-#   feed = Feedzirra::Feed.fetch_and_parse(feed_url).entries
-# end
 
 module Helpers
   def post_arrow(post, direction)
     "<a href='#{h(post.url)}'>#{direction}</a>"
+  end
+  def page_link(post)
+    h(post.url)
   end
 end
