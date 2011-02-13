a2a239371d62190c63d897781be77aef4910025e
Phil Kates <me@philkates.com>
2011-02-12 18:05:30 -0800

    Convert tiny icons to sprites using lemonade

--- config.rb
+++ config.rb
@@ -1,10 +1,11 @@
 # Require any additional compass plugins here.
+require "lemonade"
 project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "_sass"
-images_dir = "img"
+images_dir = ""
 output_style = :compact
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
--- js/recentloader.js
+++ js/recentloader.js
@@ -4,7 +4,7 @@ function add_recent_links(url) {
         $.each(json["entries"], function(i, item) {
             var service = item.via.name.toLowerCase();
             if (service == "philk's activity") {service = "github"};
-            var serviceImg = '<a href="'+item.via.url+'"><img src="/img/'+service+'.png"/></a>';
+            var serviceImg = '<a class="imglink" id="'+service+'" href="'+item.via.url+'"></a>';
             var liItem = '<article class="entry"><li>'+item.body+'</li></div>';
             var rel_date = '<time datetime="'+ item.date + '" class="date_and_time">'+jQuery.timeago(item.date)+'</div>';
             var output = serviceImg + liItem + rel_date;
